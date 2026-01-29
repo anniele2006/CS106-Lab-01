5772d5f79a2c08796b55cb4bbc5274cbaf4f394b
anniele2006 <31593011+anniele2006@users.noreply.github.com>
2026-01-29 07:50:04 +0000

    Finished ex 11

--- 11_isZero.c
+++ 11_isZero.c
@@ -9,7 +9,7 @@
  */
 int isZero(int x)
 {
-    return 2;
+    return !x;                  // !anything_nonzero = 0, !0 = 1
 }
 
 int test_isZero(int x)
@@ -19,7 +19,7 @@ int test_isZero(int x)
 
 int main(void)
 {
-    int x = 0;
+    int x = 5;
     printf("expected: %x\n", isZero(x));
     printf("actual  : %x\n", test_isZero(x));
 }
